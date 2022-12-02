1632a9eafd9e06a78b100e4fc1feb0af770ba29e
smojica22 <98427657+smojica22@users.noreply.github.com>
2022-12-01 19:22:43 -0800

    Add files via upload

---
+++ app.R
@@ -0,0 +1,108 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinythemes)
+library(dplyr)
+library(readr)
+
+data("categories")
+head(categories)
+res <- gtrends(c("Horror Films"," Animated Films"), geo = c("US"), time = "all")
+plot(res)
+res <- res$interest_over_time
+head(res)
+res$hits<- as.numeric(res$hits)
+res1<- res %>% 
+  mutate(close = (res$hits)/100) %>% 
+  rename(type = keyword) %>% 
+  select(type, date, close)
+head(res1)
+trend_description_1 <- read_csv("trend_description_1.csv")
+
+
+
+
+# Define UI ---------------------------------------------------------
+ui <- fluidPage(theme = shinytheme("lumen"),
+                titlePanel("Google Trend Index"),
+                sidebarLayout(
+                  sidebarPanel(
+                    
+                    # Select type of trend to plot
+                    selectInput(inputId = "type", label = strong("Trend index"),
+                                choices = unique(res1$type),
+                                selected = "Horror Films"),
+                    
+                    # Select date range to be plotted
+                    dateRangeInput("date", strong("Date range"), 
+                                   start = "2004-01-01", end = "2022-11-01",
+                                   min = "2004-01-01", max = "2022-11-01"),
+                    
+                    # Select whether to overlay smooth trend line
+                    checkboxInput(inputId = "smoother", 
+                                  label = strong("Overlay smooth trend line"), 
+                                  value = FALSE),
+                    
+                    # Display only if the smoother is checked
+                    conditionalPanel(condition = "input.smoother == true",
+                                     sliderInput(inputId = "f", label = "Smoother span:",
+                                                 min = 0.01, max = 1, value = 0.67, step = 0.01,
+                                                 animate = animationOptions(interval = 100)),
+                                     HTML("Higher values give more smoothness.")
+                    )
+                  ),
+                  
+                  # Output: Description, lineplot, and reference
+                  mainPanel(
+                    plotOutput(outputId = "lineplot", height = "300px"),
+                    textOutput(outputId = "desc"),
+                    tags$a(href = "https://www.google.com/finance/domestic_trends", "Source: Google Domestic Trends", target = "_blank")
+                  )
+                )
+)
+
+# Define server function --------------------------------------------
+server <- function(input, output) {
+  
+  # Subset data
+  selected_trends <- reactive({
+    req(input$date)
+    validate(need(!is.na(input$date[1]) & !is.na(input$date[2]), "Error: Please provide both a start and an end date."))
+    validate(need(input$date[1] < input$date[2], "Error: Start date should be earlier than end date."))
+    res1 %>%
+      filter(
+        type == input$type,
+        date > as.POSIXct(input$date[1]) & date < as.POSIXct(input$date[2]
+        ))
+  })
+  
+  
+  # Create scatterplot object the plotOutput function is expecting
+  output$lineplot <- renderPlot({
+    color = "#434343"
+    par(mar = c(4, 4, 1, 1))
+    plot(x = selected_trends()$date, y = selected_trends()$close, type = "l",
+         xlab = "Date", ylab = "Trend index", col = color, fg = color, col.lab = color, col.axis = color)
+    # Display only if smoother is checked
+    if(input$smoother){
+      smooth_curve <- lowess(x = as.numeric(selected_trends()$date), y = selected_trends()$close, f = input$f)
+      lines(smooth_curve, col = "#E6553A", lwd = 3)
+    }
+  })
+  
+  # Pull in description of trend
+  output$desc <- renderText({
+    trend_text <- filter(trend_description_1, type == input$type) %>% pull(text)
+    paste(trend_text, "The index is set to 1.0 on January 1, 2004 and is calculated only for US search traffic.")
+  })
+}
+
+# Create the Shiny app object ---------------------------------------
+shinyApp(ui = ui, server = server)
